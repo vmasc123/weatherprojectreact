3e289c99cd480189fdd1f2a0bba38c760ebacd51
vmasc123 <75334318+vmasc123@users.noreply.github.com>
2021-03-07 16:50:24 -0500

    Created Forecast.js and WeatherForecast.js

---
+++ src/Forecast.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
+import WeatherForecastPreview from "./WeatherForecastPreview";
+
+export default function Forecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+    console.log(response.data);
+  }
+
+  if (loaded && props.city === forecast.city.name) {
+    return (
+      <div className="row">
+        <WeatherForecastPreview data={forecast.list[0]} unit="celsius" />
+        <WeatherForecastPreview data={forecast.list[1]} unit="celsius" />
+        <WeatherForecastPreview data={forecast.list[2]} unit="celsius" />
+        <WeatherForecastPreview data={forecast.list[3]} unit="celsius" />
+        <WeatherForecastPreview data={forecast.list[4]} unit="celsius" />
+      </div>
+    );
+  } else {
+    let apiKey = "4235c1e76fa14f9f8542da028429e01a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleForecastResponse);
+
+    return null;
+  }
+}
---
+++ src/WeatherForecastPreview.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+
+export default function WeatherForecastPreview(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    return `${hours}:00`;
+  }
+
+  function temperature() {
+    let temperature = Math.round(props.data.main.temp);
+    return `${temperature}°C`;
+  }
+
+  if (props.unit === "celsius") {
+    return (
+      <div className="colRow">
+        <h3 className="time">{hours()}</h3>
+        <img
+          className="forecastIcons"  
+          src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
+          alt={[props.data.main.temp]}
+        />
+        <p className="forecastTemperature"> {temperature()} </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="colRow">
+        <h3 className="time">{hours()}</h3>
+        <img
+          className="forecastIcons"   
+          src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
+          alt={[props.data.main.temp]}
+        />
+      </div>
+    );
+  }
+}
